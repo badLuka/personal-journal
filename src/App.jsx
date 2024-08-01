caa30b1aa2b6256e5dfa40dd0aa0823ad2533fd0
mk <mkalugin21@gmail.com>
2024-08-01 20:17:35 +0400

    add useState for Button and to decomposotion on components

--- src/App.jsx
+++ src/App.jsx
@@ -5,41 +5,41 @@ import JournalItem from './components/JournalItem/JournalItem';
 
 function App() {
 
-    const data = [
-        {
-            title: 'Подготовка к обновлению курсов',
-            text: 'Горные походы открывают удивительные природные ландшафты',
-            date: new Date()
+	const data = [
+		{
+			title: 'Подготовка к обновлению курсов',
+			text: 'Горные походы открывают удивительные природные ландшафты',
+			date: new Date()
         
-        },
-        {
-            title: 'Поход в горы',
-            text: 'Думал, что очень много времени',
-            date: new Date()
+		},
+		{
+			title: 'Поход в горы',
+			text: 'Думал, что очень много времени',
+			date: new Date()
           
-        }
-    ];
+		}
+	];
 
-    return (
-        <>
-            <h1>Заголовок</h1>
-            <p>Текст</p>
-            <Button />
-            <CardButton>
-                <JournalItem
-                    title={data[0].title}
-                    text={data[0].text}
-                    date={data[0].date}
-                />
-            </CardButton>
+	return (
+		<>
+			<h1>Заголовок</h1>
+			<p>Тут какой-то текст!</p>
+			<Button />
+			<CardButton>
+				<JournalItem
+					title={data[0].title}
+					text={data[0].text}
+					date={data[0].date}
+				/>
+			</CardButton>
 
-            <JournalItem
-                title={data[1].title}
-                text={data[1].text}
-                date={data[1].date}
-            />
-        </>
-    );
+			<JournalItem
+				title={data[1].title}
+				text={data[1].text}
+				date={data[1].date}
+			/>
+		</>
+	);
 }
 
 export default App;
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,10 +1,17 @@
 import './Button.css';
+import { useState } from 'react';
 
 function Button() {
+	
+	const [text, setText ] = useState('Сохранить');
+	const clicked = () => {
+		setText('Закрыть');
+		console.log('Привет!');
+	};
 
-    return (
-        <button className='button accent'>Сохранить</button>
-    );
+	return (
+		<button onClick={clicked} className='button accent'>{text}</button>
+	);
 }
 
 export default Button;
