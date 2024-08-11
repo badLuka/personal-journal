798ef5ca651f7e74653bbe39da7db05b0d730ae7
mk <mkalugin21@gmail.com>
2024-08-11 18:45:22 +0400

    adding and rendering list items

--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -1,31 +1,22 @@
 import Button from '../Button/Button';
 import './JournalForm.css';
-import {useState} from 'react';
 
-function JournalForm() {
-
-	const [inputData, setInputData] = useState('');
-
-	const inputChange = (event) => {
-		setInputData(event.target.value);
-		console.log(inputData);
-	};
-
+function JournalForm({ onSubmit }) {
 	const addJournalItem = (e) => {
 		e.preventDefault();
 		const formData = new FormData(e.target);
 		const formProps = Object.fromEntries(formData);
-		console.log(formProps);
+		onSubmit(formProps);
 	};
 
 	return (
 		<form className='journal-form' onSubmit={addJournalItem}>
 			<input type="text" name='title' />
-			<input type="date" name='date'/>
-			<input type="text" name='tag' value={inputData} onChange={inputChange}/>
-			<textarea name="post" id="" cols="30" rows="10"></textarea>
-			<Button text="Сохранить" onClick={() => {console.log('Меня нажали');}} />
-		</form>		
+			<input type="date" name='date' />
+			<input type="text" name='tag' />
+			<textarea name="text" id="" cols="30" rows="10"></textarea>
+			<Button text="Сохранить" />
+		</form>
 	);
 
 }
